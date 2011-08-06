9f116eec47f05ea62bd90daab2ffe136efd5bde1
Amy Wagner <amyleannewagner@gmail.com>
2011-08-06 12:35:53 +0100

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penguin::Application.config.secret_token = 'c6a341075a7acb340cc3bae3d86ce3ff62ff77a66c36fe8e498227edbb94f2ab5ce1dcb6ba97784227434f70285a0e3ca28ba1de1527a1a7502864dcc028deaa'
